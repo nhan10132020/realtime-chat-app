7ff5cd229b9a23ddfc5d5a394c2e199c957cbf78
tranthanhnhan <cutin17vn@gmail.com>
2022-10-23 14:51:02 +0700

    handle google login

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import {auth} from '../../firebase/config'
-import {  signInWithPopup, FacebookAuthProvider } from "firebase/auth";
+import {  signInWithPopup, FacebookAuthProvider,GoogleAuthProvider } from "firebase/auth";
+
 import { addDocument, generateKeywords } from '../../firebase/services';
 import './login.css'
 import { FacebookFilled,GoogleCircleFilled } from '@ant-design/icons';
@@ -8,7 +9,7 @@ import { FacebookFilled,GoogleCircleFilled } from '@ant-design/icons';
 
 
 const fbProvider =  new FacebookAuthProvider();
-
+const ggProvider = new GoogleAuthProvider()
 const Login = () => {
 
     const handleFbLogin = async ()=>{
@@ -25,6 +26,21 @@ const Login = () => {
         }
     }
 
+    const handleGgLogin = async ()=>{
+        const {user,_tokenResponse}= await signInWithPopup(auth, ggProvider);
+          if(_tokenResponse.isNewUser){
+              addDocument('users',{
+                  displayName: user.displayName,
+                  keywords: generateKeywords(user.displayName),
+                  email: user.email,
+                  photoURL: user.photoURL,
+                  uid: user.uid,
+                  providerId: user.uid
+              })
+          }
+      }
+    
+
     return (
         <div className='box'>
             <div className='content__login'>
@@ -33,7 +49,7 @@ const Login = () => {
                     <button onClick={handleFbLogin} className='content__button content__button__facebook' style={{border:0}}><FacebookFilled style={{color:'white', marginRight:'10px', fontSize:'1rem'}}/><span style={{color:'white'}}>Continue with <span style={{fontWeight:'800'}}>Facebook</span></span></button>
                 </div>
                 <div className='content__buttons '>
-                    <button className='content__button content__button__google' style={{border:0}}><GoogleCircleFilled style={{color:'white', marginRight:'10px', fontSize:'1rem'}}/><span style={{color:'white'}}>Continue with <span style={{fontWeight:'800'}}>Google</span></span></button>
+                    <button onClick={handleGgLogin} className='content__button content__button__google' style={{border:0}}><GoogleCircleFilled style={{color:'white', marginRight:'10px', fontSize:'1rem'}}/><span style={{color:'white'}}>Continue with <span style={{fontWeight:'800'}}>Google</span></span></button>
                 </div>
             </div>
         </div>
